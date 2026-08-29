9078be90afe394fdffc80fc05cc0aad0e57f8936
TaePaworn <taepaworn@gmail.com>
2026-08-30 00:27:37 +0700

    add button music & upgrade lobby music

--- scenes/texture_button.gd
+++ scenes/texture_button.gd
@@ -1,23 +1,22 @@
 extends TextureButton
 
-# สร้างช่องใน Inspector สำหรับใส่รูป
 @export var icon_sound_on: Texture2D
 @export var icon_sound_off: Texture2D
 
-var master_bus_index: int
+var target_bus_index: int
 
 func _ready():
-	master_bus_index = AudioServer.get_bus_index("Master")
+	# เปลี่ยนเป้าหมายจาก Master มาเป็นช่อง GameSound ที่เพิ่งสร้างใหม่
+	target_bus_index = AudioServer.get_bus_index("GameSound")
 	_update_button_icon()
 
 func _on_pressed():
-	var is_muted = AudioServer.is_bus_mute(master_bus_index)
-	AudioServer.set_bus_mute(master_bus_index, not is_muted)
+	var is_muted = AudioServer.is_bus_mute(target_bus_index)
+	AudioServer.set_bus_mute(target_bus_index, not is_muted)
 	_update_button_icon()
 
 func _update_button_icon():
-	# สลับรูปภาพตามสถานะเสียง
-	if AudioServer.is_bus_mute(master_bus_index):
+	if AudioServer.is_bus_mute(target_bus_index):
 		texture_normal = icon_sound_off
 	else:
 		texture_normal = icon_sound_on
